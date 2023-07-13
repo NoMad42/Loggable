8635a23efe67a55353e61a9dba114cdcea4a4470
NoMad42 <NoMad42@users.noreply.github.com>
2023-07-13 15:28:52 +0000

    Fix styling

--- src/Loggable.php
+++ src/Loggable.php
@@ -5,6 +5,7 @@ namespace NoMad42\Loggable;
 interface Loggable
 {
     public function getLevel(): string;
+
     public function getMessage(): string|\Stringable;
 
     public function getContext(): array;
