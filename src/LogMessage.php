5c3741268ed264d72ded9f3439b0649f7b93f436
NoMad42 <nomad42@nomad42.ru>
2023-08-01 14:25:03 +0300

    added makeFromException helper method

--- src/LogMessage.php
+++ src/LogMessage.php
@@ -2,6 +2,8 @@
 
 namespace NoMad42\Loggable;
 
+use Psr\Log\LogLevel;
+
 class LogMessage implements Loggable
 {
     public function __construct(
@@ -24,5 +26,18 @@ class LogMessage implements Loggable
     public function getContext(): array
     {
         return $this->context;
+    }
+
+    public static function makeFromException(
+        \Exception $exception,
+        string $level = null,
+        string|\Stringable $message = null,
+        array $context = []
+    ): LogMessage {
+        return new LogMessage(
+            $level ?? LogLevel::ERROR,
+            $message ?? $exception->getMessage(),
+            ['exception' => $exception] + $context,
+        );
     }
 }
--- tests/LogMessageTest.php
+++ tests/LogMessageTest.php
@@ -14,3 +14,39 @@ it('can be logged in null logger', function () {
         $logMessage->getContext()
     );
 })->expectNotToPerformAssertions();
+
+it('can be made from exception', function () {
+    $logger = new \Psr\Log\NullLogger();
+    $logMessage = \NoMad42\Loggable\LogMessage::makeFromException(
+        new Exception('Oh no!')
+    );
+
+    $logger->log(
+        $logMessage->getLevel(),
+        $logMessage->getMessage(),
+        $logMessage->getContext()
+    );
+})->expectNotToPerformAssertions();
+
+it('can reassign fields value by method arguments', function () {
+    $level = \Psr\Log\LogLevel::CRITICAL;
+    $message = 'Critical error!';
+    $context = [
+        'exception' => 'should not replace value',
+        'key' => ' some value',
+    ];
+    $exception = new Exception('Oh no!');
+
+    $logMessage = \NoMad42\Loggable\LogMessage::makeFromException(
+        $exception,
+        $level,
+        $message,
+        $context,
+    );
+
+    $context['exception'] = $exception;
+
+    expect($logMessage->getLevel())->toBe($level);
+    expect($logMessage->getMessage())->toBe($message);
+    expect($logMessage->getContext())->toBe($context);
+});
